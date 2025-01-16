957d3ec05dd534f634e7853801c30550cb5a6c61
amirkhan1092 <amirkhan1092@gmail.com>
2025-01-17 00:14:59 +0530

    Documentation Update

--- 01_OOP_Concepts/01_Introduction_to_OOP/Examples/employee_management.py
+++ 01_OOP_Concepts/01_Introduction_to_OOP/Examples/employee_management.py
@@ -1,42 +1,73 @@
 # Employee Management System 
-
 class Employee:
-    def __init__(self, name, emp_id, deptt, salary):
-        self.name = name
+    def __init__(self,  emp_id, name, deptt, salary):
         self.emp_id = emp_id
+        self.name = name
         self.deptt = deptt
         self.salary = salary
-    def display_info(self):
-        st = f'''Name: {self.name}
-Employee Id: {self.emp_id}
+    def get_details(self):
+        return f'''Employee Id: {self.emp_id}
+Name: {self.name}
 Department: {self.deptt}
 Salary: {self.salary}'''
-        print(st)
-    def update_info(self, name=None, deptt=None, salary=None):
+
+    def update_details(self, name=None, deptt=None, salary=None):
         if name:
             self.name = name
         if deptt:
             self.deptt = deptt
         if salary:
             self.salary = salary
-    def increment_salary(self,perc):
-        self.salary += (perc/100) *self.salary
+    def increment_salary(self, prec):
+        self.salary += (prec/100 * salary)
 
 
-class Employee_management:
+
+
+class Employee_Management:
     def __init__(self):
+        print('Management tools initialized \nSystem Ready\n\n')
         self.reg = {}
-    def add_employee(self, name, emp_id, deptt, salary):
-        if emp_id in self.reg:
-            print('Employee id already exist')
-        else:
-            self.reg[emp_id] = Employee(name, emp_id, deptt, salary)
-            print(f'Employee Id {emp_id} successfully created')
-    def display_all_employee(self):
-        for emp in self.reg:
-            self.reg[emp].display_info()
 
-# Main Code
-systm = Employee_management()
-systm.add_employee('Ravi', 'gla11763', 'cse', 10000)
-systm.add_employee('Saket', 'gla18863', 'ece', 1000)
+    def add_employee(self, emp_id, name, deptt, salary):
+        if emp_id in self.reg:
+            print('Employee already exist')
+            print('check details Try with different Employee ID')
+            print('Already filled details')
+            print(self.reg[emp_id].get_details)
+        else:
+            self.reg[emp_id] = Employee(emp_id, name, deptt, salary) 
+    
+
+
+
+# main code 
+
+print('Welcome to GLA University\n\n')
+
+system = Employee_Management()
+
+print('Available Choice\n')
+print('''
+1. Add Employee
+2. Update Employee Info
+3. Display All Employees
+4. Delete Employee by ID 
+5. Salary Increment by ID
+6. Exit
+''')
+choice = int(input('Enter the choice [1-6]'))
+if choice == 1:
+    e_id = input('Enter the Employee ID')
+    name = input('Enter the Employee Name')
+    dept = input('Enter the Employee Department')
+    salary = int(input('Enter the Salary '))
+
+    system.add_employee(e_id, name, dept, salary)
+
+
+
+
+
+
+
